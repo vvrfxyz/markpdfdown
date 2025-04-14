a8678d02a5a3eba1dff27c1b5ef1217fd13139b9
ruifeng.wen <vvrfxyz@gmail.com>
2025-04-14 05:24:59 +0000

    init

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from core.dataset import PymuDocDataset
 from core.doc_analyze import doc_analyze
 
-pdf_file_path = "/Users/vvrfxyz/PycharmProjects/vvrfxyz/markpdfdown/PMC11742413_pages_1-3.pdf"
+pdf_file_path = "/data/markpdfdown/PMC11742413_pages_1-3.pdf"
 # pdf_bytes = ""
 with open(pdf_file_path, 'rb') as f:
     pdf_bytes = f.read()
